e8f2c05d31f6dd59c2dbf15d0e0efb58d8fa94eb
Jeff Terry <terryj@iit.edu>
2020-04-12 12:27:56 -0500

    Initial Commit

---
+++ CatalystPlot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatalystPlot
+//
+//  Created by Jeff Terry on 4/12/20.
+//  Copyright © 2020 Jeff Terry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
